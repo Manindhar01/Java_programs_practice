50ab71737d66379a034590ad50ca4ed13c4764df
Manindhar1 <manintharvlr59@gmail.com>
2024-06-05 18:53:54 +0530

    first commit

---
+++ java_program/src/java_program/_03GivenNumOddOrEven.java
@@ -0,0 +1,18 @@
+package java_program;
+
+public class _03GivenNumOddOrEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=47;
+		switch(n%2) {
+		case 0:
+			System.out.println("even number");
+			break;
+		case 1:
+			System.out.println("odd number");
+		}
+
+	}
+
+}
