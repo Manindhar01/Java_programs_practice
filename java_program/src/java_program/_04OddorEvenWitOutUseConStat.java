cdd30a11dddac0627a12b05543fca5472e8471ac
Manindhar1 <manintharvlr59@gmail.com>
2024-06-06 15:45:30 +0530

    first commit

---
+++ java_program/src/java_program/_04OddorEvenWitOutUseConStat.java
@@ -0,0 +1,14 @@
+package java_program;
+
+public class _04OddorEvenWitOutUseConStat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=87;
+		int b=a%2;
+		String c=b==0?"Even number":"odd number";
+		System.out.println(c);
+
+	}
+
+}
