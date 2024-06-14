35ef3d9b27eadf65b9944bf5e436a094ac058123
Manindhar1 <manintharvlr59@gmail.com>
2024-06-14 10:59:24 +0530

    first commit

---
+++ java_program/src/java_program/_05OddorEvenWitOutUseModorDiv.java
@@ -0,0 +1,16 @@
+package java_program;
+
+public class _05OddorEvenWitOutUseModorDiv {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=45;
+		if((n&=1)==1) {
+			System.out.println("odd number");
+		}else {
+			System.out.println("even number");
+		}
+
+	}
+
+}
